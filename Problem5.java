40b56cfd38f399278b93526d030f43fb3b20c8fa
Arafat Rahman <139953232+arafat-rahman17@users.noreply.github.com>
2023-08-09 13:23:42 +0600

    Problem5.java

---
+++ Problem5.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Problem5 {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        int N = read.nextInt();
+
+			for(int i =1;i<=10;i++){
+				System.out.println(N+" x "+i +" = "+ (N*i));
+			}
+	}
+}
